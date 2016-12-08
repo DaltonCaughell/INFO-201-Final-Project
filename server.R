c668f75b182e7bcdcc303e6b99b795f5c1563692
Dalton Caughell <daltoncaughell@gmail.com>
2016-12-08 09:50:20 -0800

    Adding some doc for the weirdness

--- server.R
+++ server.R
@@ -123,6 +123,8 @@ shinyServer(function(input, output, session) {
     map.df$state.name <- tolower(map.df$state.name)
     
     map.df <- merge(states,map.df, by.x="region", by.y="state.name", all.x=T)
+    
+    # Need to re-order the data to prevent polygons from being drawn twisted
     map.df <- map.df[order(map.df$order),]
     
     heatmap <- ggplot(map.df, aes(x=long,y=lat,group=group))+
