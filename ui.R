8c2244c0ef8eccb50c80cfed80c9de0d991242b2
Dalton Caughell <daltoncaughell@gmail.com>
2016-12-08 23:38:43 -0800

    Adding final section, closes #6, closes #2

--- ui.R
+++ ui.R
@@ -67,6 +67,27 @@ shinyUI(navbarPage("Natural Disaster Information",
         plotlyOutput("worldscatterplot")
       )
     )
+  ),
+  tabPanel(
+    "Disasters",
+    sidebarLayout(
+      sidebarPanel(
+        sliderInput("bins",
+                    "Number of bins:",
+                    min = 1,
+                    max = 50,
+                    value = 30),
+        selectInput("indicatorInput", "Disaster Type",choices = levels(dat$disaster.type)[-c(1,7,8)])
+      ),
+      
+      # Show a plot of the generated distribution
+      mainPanel(
+        h1("Histogram"),
+        plotOutput("distPlot"),br(),
+        h1("Summary"),
+        verbatimTextOutput("summary"), width = 7
+      )
+    )
   )
   )
 )
