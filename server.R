c2a0bd3c065f0e16fe34d85de6089fb02e71bf12
Dalton Caughell <daltoncaughell@gmail.com>
2016-12-08 23:44:32 -0800

    Fixing bug with var name

--- server.R
+++ server.R
@@ -190,16 +190,16 @@ shinyServer(function(input, output, session) {
   
   output$summary<- renderPrint({
     # Disasters Data INIT
-    dat <- data.frame(read.csv("data/disasters.csv"))
-    filtered <- dat %>% 
+    DisasterData <- data.frame(read.csv("data/disasters.csv"))
+    filtered <- DisasterData %>% 
       filter(disaster.type == input$indicatorInput)
     print(summary(filtered$Total.deaths))
     #ggplot(filtered, aes(value))+geom_histogram()
-    
   })
+  
   output$distPlot <- renderPlot({
     # Disasters Data INIT
-    dat <- data.frame(read.csv("data/disasters.csv"))
+    DisasterData <- data.frame(read.csv("data/disasters.csv"))
     # if(input$indicatorInput == "Animal accident"){
     # x    <- dat[dat$disaster.type=='Animal accident',]
     # bins <- seq(min(x), max(x), length.out = input$bins + 20)
@@ -208,7 +208,7 @@ shinyServer(function(input, output, session) {
     # ggplot(data = x, aes(x))+geom_histogram(binwidth=100, colour="black", fill="white")
     # }
     if(input$indicatorInput == "Drought"){
-      x    <- dat[dat$disaster.type=="Drought",]$Total.deaths 
+      x    <- DisasterData[DisasterData$disaster.type=="Drought",]$Total.deaths 
       bins <- seq(min(x, na.rm = T), max(x, na.rm = T), length.out = input$bins + 20)
       # draw the histogram with the specified number of bins
       hist(x, col = 'darkblue', border = 'white', xlab = "Total Deaths", main = "Histogram of Total deaths in Drought") 
@@ -216,28 +216,28 @@ shinyServer(function(input, output, session) {
     }
     
     else if(input$indicatorInput == "Earthquake"){
-      x    <- dat[dat$disaster.type=="Earthquake",]$Total.deaths 
+      x    <- DisasterData[DisasterData$disaster.type=="Earthquake",]$Total.deaths 
       bins <- seq(min(x, na.rm = T), max(x, na.rm = T), length.out = input$bins + 20)
       # draw the histogram with the specified number of bins
       hist(x, breaks = bins, col = 'darkblue', border = 'white', xlab = "Total Deaths", main = "Histogram of Total deaths in Earthquake") 
     }
     
     else if(input$indicatorInput == "Epidemic"){
-      x    <- dat[dat$disaster.type=="Epidemic",]$Total.deaths 
+      x    <- DisasterData[DisasterData$disaster.type=="Epidemic",]$Total.deaths 
       bins <- seq(min(x, na.rm = T), max(x, na.rm = T), length.out = input$bins + 20)
       # draw the histogram with the specified number of bins
       hist(x, breaks = bins, col = 'darkblue', border = 'white', xlab = "Total Deaths", main = "Histogram of Total deaths in Epidemic") 
     }
     
     else if(input$indicatorInput == "Extreme temperature "){
-      x    <- dat[dat$disaster.type=="Extreme temperature ",]$Total.deaths 
+      x    <- DisasterData[DisasterData$disaster.type=="Extreme temperature ",]$Total.deaths 
       bins <- seq(min(x, na.rm = T), max(x, na.rm = T), length.out = input$bins + 20)
       # draw the histogram with the specified number of bins
       hist(x, breaks = bins, col = 'darkblue', border = 'white', xlab = "Total Deaths", main = "Histogram of Total deaths in Extreme temperature ") 
     }
     
     else if(input$indicatorInput == "Flood"){
-      x    <- dat[dat$disaster.type=="Flood",]$Total.deaths 
+      x    <- DisasterData[DisasterData$disaster.type=="Flood",]$Total.deaths 
       bins <- seq(min(x, na.rm = T), max(x, na.rm = T), length.out = input$bins + 20)
       # draw the histogram with the specified number of bins
       hist(x, breaks = bins, col = 'darkblue', border = 'white', xlab = "Total Deaths", main = "Histogram of Total deaths in Flood") 
@@ -258,42 +258,42 @@ shinyServer(function(input, output, session) {
     # }
     
     else if(input$indicatorInput == "Insect infestation"){
-      x    <- dat[dat$disaster.type=="Insect infestation",]$Total.deaths 
+      x    <- DisasterData[DisasterData$disaster.type=="Insect infestation",]$Total.deaths 
       bins <- seq(min(x, na.rm = T), max(x, na.rm = T), length.out = input$bins + 20)
       # draw the histogram with the specified number of bins
       hist(x, breaks = bins, col = 'darkblue', border = 'white', xlab = "Total Deaths", main = "Histogram of Total deaths in Insect infestation") 
     }
     
     else if(input$indicatorInput == "Landslide"){
-      x    <- dat[dat$disaster.type=="Landslide",]$Total.deaths 
+      x    <- DisasterData[DisasterData$disaster.type=="Landslide",]$Total.deaths 
       bins <- seq(min(x, na.rm = T), max(x, na.rm = T), length.out = input$bins + 20)
       # draw the histogram with the specified number of bins
       hist(x, breaks = bins, col = 'darkblue', border = 'white', xlab = "Total Deaths", main = "Histogram of Total deaths in Landslide") 
     }
     
     else if(input$indicatorInput == "Mass movement (dry)"){
-      x    <- dat[dat$disaster.type=="Mass movement (dry)",]$Total.deaths 
+      x    <- DisasterData[DisasterData$disaster.type=="Mass movement (dry)",]$Total.deaths 
       bins <- seq(min(x, na.rm = T), max(x, na.rm = T), length.out = input$bins + 20)
       # draw the histogram with the specified number of bins
       hist(x, breaks = bins, col = 'darkblue', border = 'white', xlab = "Total Deaths", main = "Histogram of Total deaths in Mass movement (dry)") 
     }
     
     else if(input$indicatorInput == "Storm"){
-      x    <- dat[dat$disaster.type=="Storm",]$Total.deaths 
+      x    <- DisasterData[DisasterData$disaster.type=="Storm",]$Total.deaths 
       bins <- seq(min(x, na.rm = T), max(x, na.rm = T), length.out = input$bins + 20)
       # draw the histogram with the specified number of bins
       hist(x, breaks = bins, col = 'darkblue', border = 'white', xlab = "Total Deaths", main = "Histogram of Total deaths in Storm") 
     }
     
     else if(input$indicatorInput == "Volcanic activity"){
-      x    <- dat[dat$disaster.type=="Volcanic activity",]$Total.deaths 
+      x    <- DisasterData[DisasterData$disaster.type=="Volcanic activity",]$Total.deaths 
       bins <- seq(min(x, na.rm = T), max(x, na.rm = T), length.out = input$bins + 20)
       # draw the histogram with the specified number of bins
       hist(x, breaks = bins, col = 'darkblue', border = 'white', xlab = "Total Deaths", main = "Histogram of Total deaths in Volcanic activity") 
     }
     
     else{
-      x    <- dat[dat$disaster.type=="Wildfire",]$Total.deaths 
+      x    <- DisasterData[DisasterData$disaster.type=="Wildfire",]$Total.deaths 
       bins <- seq(min(x, na.rm = T), max(x, na.rm = T), length.out = input$bins + 20)
       # draw the histogram with the specified number of bins
       hist(x, breaks = bins, col = 'darkblue', border = 'white', xlab = "Total Deaths", main = "Histogram of Total deaths in Wildfire") 
